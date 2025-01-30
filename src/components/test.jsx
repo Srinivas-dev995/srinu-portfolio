54331a7f04172af00a60ab5b28dfecba13e6f4b8
Srinivasa Reddy Elakoti <elakotisrinu48@gmail.com>
2025-01-30 16:46:33 +0530

    by srinu

---
+++ src/components/test.jsx
@@ -0,0 +1,70 @@
+// const a = 12;
+// console.log(a*12)
+// console.log(Math.pow(a, 12))
+
+// function test(){
+//     salary = 1200;
+//     console.log(salary)
+// }
+// test()
+
+// function test(record){
+//     if(record == {age:28}){
+//         console.log("adult");
+//     }else if(record === {age:38})
+//         console.log("still adukt");
+//     }else{
+//         console.log("No record found");
+//     }
+// }
+
+// test({age:28});
+
+// var a = {};
+// var b = {key:"b"};
+// var c = {key:"c"};
+
+// a[b] = 600  // 
+// b[c] = 700
+
+// console.log(a[c]) // 
+// console.log(a[b])
+// console.log(b[b])
+// console.log(b[c])
+
+let s1 = "abcdef"
+let s2 = 123
+// op : a1b2c3def
+
+let s3 = "abc"
+let s4 = 123456
+// op : a1b2c3456
+
+
+// we should check which one is greater then devide it by half
+
+// function stringMan(s1,s2){
+//     s1 = String(s1);
+//     s2 = String(s2);
+    
+//     const len1 = s1.length;
+//     const len2 = s2.length;
+    
+   
+//     const greater = len1 > len2 ? s1 : s2;
+//     const smaller = len1 <= len2 ? s1 : s2;
+//     let i = 0;
+//     const half =  greater.length / 2;
+//     let result = "";
+//     while(i < half){
+//          result += greater[i] + smaller[i];
+//         i++;
+//     }
+//        result += greater.slice(i);
+
+//     console.log(result);
+//     }
+
+
+// stringMan("abcdef", 123); // Output: a1b2c3def
+// stringMan("abc", 123456);  // Output: a1b2c3456
